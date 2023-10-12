68ed692d6c1ad1b02ab350cc3acc1aad88b45701
yuseongmok <202313006@g.shingu.ac.kr>
2023-10-12 11:24:57 +0900

    타워 공격시스템

---
+++ VR02/Assets/Scripts/Tower_Siystem/Projectile.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    private Rigidbody rb;                   //내부에서 사용할 rb
+
+    public float moveSpeed;                 //이동속도
+    public float damagedAmount;             //데미지 량
+    private bool hasDamaged;                //flag선언
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.velocity = transform.forward * moveSpeed;                //rb의 앞쪽 방향으로 총알 이동 속도를 입력
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Enemy" && !hasDamaged)
+        {
+            hasDamaged = true;
+        }
+
+        Destroy(gameObject);                                        //Trigger 충돌이 일어나면 파괴
+    }
+}
